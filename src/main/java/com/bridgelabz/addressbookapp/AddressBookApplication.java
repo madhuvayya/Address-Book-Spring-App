532121cf182f3c4c9f76117f2be4a5d06e9bfead
madhuvayya <madhu.vayya@gmail.com>
2020-12-23 23:10:38 +0530

    Initialized project setup

---
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookApplication.class, args);
+    }
+
+}
