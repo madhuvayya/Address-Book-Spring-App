e866de40c7837e08ef7e7cc1d9914b83d5ada63c
madhuvayya <madhu.vayya@gmail.com>
2020-12-25 22:56:04 +0530

    Added separate log properties files for development and production

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
@@ -3,14 +3,16 @@ package com.bridgelabz.addressbookapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class AddressBookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookApplication.class, args);
-        log.info("AddressBook App started");
+        ApplicationContext context =  SpringApplication.run(AddressBookApplication.class, args);
+        log.info("AddressBook App started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
     }
 
 }
