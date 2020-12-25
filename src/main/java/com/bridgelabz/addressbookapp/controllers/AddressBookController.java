1fa442c07c4fbeb60cc6ae4b240dbc65cc69ec29
madhuvayya <madhu.vayya@gmail.com>
2020-12-25 22:38:32 +0530

    Added slf4j annotation to log the application

--- src/main/java/com/bridgelabz/addressbookapp/controllers/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controllers/AddressBookController.java
@@ -4,6 +4,7 @@ import com.bridgelabz.addressbookapp.dto.ResponseDTO;
 import com.bridgelabz.addressbookapp.model.Contact;
 import com.bridgelabz.addressbookapp.dto.ContactDTO;
 import com.bridgelabz.addressbookapp.service.IAddressBookService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/addressbook")
+@Slf4j
 public class AddressBookController {
 
     @Autowired
@@ -34,7 +36,9 @@ public class AddressBookController {
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createContact(@RequestBody ContactDTO contactDTO){
+        log.debug("Received request for creating new contact with data:",contactDTO.toString());
         Contact contact = addressBookService.createContact(contactDTO);
+        log.debug("New contact is created.");
         ResponseDTO responseDTO = new ResponseDTO("POST REST call for creating contact is successful",contact);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
     }
@@ -42,14 +46,18 @@ public class AddressBookController {
     @PutMapping("/update/{contactId}")
     public ResponseEntity<ResponseDTO> updateContact(@PathVariable("contactId") int contactId,
                                                      @RequestBody ContactDTO contactDTO){
+        log.debug("Received request for updating existing contact with data:",contactDTO.toString());
         Contact contact = addressBookService.updateContact(contactId,contactDTO);
+        log.debug("Contact is Updated");
         ResponseDTO responseDTO = new ResponseDTO("PUT RETS call for updating contact is successful",contact);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{contactId}")
     public ResponseEntity<ResponseDTO> deleteContact(@PathVariable("contactId") int contactId){
+        log.debug("Received request for deleting contact with id:", contactId);
         addressBookService.deleteContact(contactId);
+        log.debug("Contact "+ contactId +" deleted.");
         ResponseDTO responseDTO = new ResponseDTO("DELETE REST call for deleting contact is successful","Deleted Id: "+ contactId);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
     }
